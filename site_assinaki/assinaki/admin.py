20f22b8501fe88cdf13cd82248aab51f82e921a7
tmenegaz <tmenegaz77@gmail.com>
2018-11-26 13:01:43 -0300

    Teste no modelo

--- site_assinaki/assinaki/admin.py
+++ site_assinaki/assinaki/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Pais
 
 # Register your models here.
+class AdminAssinaki(admin.ModelAdmin):
+    list_display = ["pais_nome"]
+
+class Meta:
+    model = Pais
+
+admin.site.register(Pais, AdminAssinaki)
--- site_assinaki/assinaki/models.py
+++ site_assinaki/assinaki/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import ForeignKey
+
+
+class Pais(models.Model):
+    pais_nome = models.CharField(max_length=50)
+
+class Uf(models.Model):
+    uf_nome = models.CharField(max_length=50)
+    pais = models.ForeignKey('Pais', on_delete=models.DO_NOTHING)
+
+class Cidade(models.Model):
+    cidade_nome = models.CharField(max_length=50)
+    uf = models.ForeignKey('Uf', on_delete=models.DO_NOTHING)
+
