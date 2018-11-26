aacfc8452d3386b0eb43bb4c5a6355cfeec6ef48
tmenegaz <tmenegaz77@gmail.com>
2018-11-26 15:32:29 -0300

    model e admin

--- site_assinaki/assinaki/admin.py
+++ site_assinaki/assinaki/admin.py
@@ -4,6 +4,14 @@ from .models import Pais, Uf, Cidade
 
 # Register your models here.
 
-@admin.register(Pais, Uf, Cidade)
-class AdminAssinaki(admin.ModelAdmin):
-    pass
+@admin.register(Pais)
+class Pais_admin(admin.ModelAdmin):
+    list_display = ['pais_nome']
+
+@admin.register(Uf)
+class Uf_admin(admin.ModelAdmin):
+    list_display = ['uf_nome']
+
+@admin.register(Cidade)
+class Cidade_admin(admin.ModelAdmin):
+    list_display = ['cidade_nome']
--- site_assinaki/assinaki/models.py
+++ site_assinaki/assinaki/models.py
@@ -5,23 +5,35 @@ from django.db.models import ForeignKey
 
 
 class Pais(models.Model):
-    pais_nome = models.CharField(max_length=50)
+    pais_nome = models.CharField('Nome do país',max_length=50)
 
     def __str__(self):
         return self.pais_nome
 
+    class Meta:
+        verbose_name = 'País'
+        verbose_name_plural = "Países"
+
 class Uf(models.Model):
-    uf_nome = models.CharField(max_length=50)
+    uf_nome = models.CharField('Nome do  estado', max_length=50)
     pais = models.ForeignKey('Pais', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.uf_nome
 
+    class Meta:
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados'
+
 class Cidade(models.Model):
-    cidade_nome = models.CharField(max_length=50)
+    cidade_nome = models.CharField('Nome da cidade', max_length=50)
     uf = models.ForeignKey('Uf', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.cidade_nome
+
+    class Meta:
+        verbose_name = 'Cidade'
+        verbose_name_plural = "Cidades"
 
 
