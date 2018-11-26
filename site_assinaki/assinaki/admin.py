dfb78e44eee6f0262d76fe8677ac9d92b5d07098
tmenegaz <tmenegaz77@gmail.com>
2018-11-26 16:21:37 -0300

    model e admin

--- site_assinaki/assinaki/admin.py
+++ site_assinaki/assinaki/admin.py
@@ -10,8 +10,12 @@ class Pais_admin(admin.ModelAdmin):
 
 @admin.register(Uf)
 class Uf_admin(admin.ModelAdmin):
-    list_display = ['uf_nome']
+    list_display = ['uf_nome', 'pais']
+    list_display_links = ['pais']
+    list_select_related = ['pais']
 
 @admin.register(Cidade)
 class Cidade_admin(admin.ModelAdmin):
-    list_display = ['cidade_nome']
+    list_display = ['cidade_nome', 'uf']
+    list_display_links = ['uf']
+    list_select_related = ['uf']
