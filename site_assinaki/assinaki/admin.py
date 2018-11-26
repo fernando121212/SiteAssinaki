908d829a1d7cd563cecc179242cb5a4f10ea4189
tmenegaz <tmenegaz77@gmail.com>
2018-11-26 16:48:29 -0300

    model e admin

--- site_assinaki/assinaki/admin.py
+++ site_assinaki/assinaki/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pais, Uf, Cidade
+from .models import Pais, Uf, Cidade, Bairro, Rua
 
 
 # Register your models here.
@@ -19,3 +19,15 @@ class Cidade_admin(admin.ModelAdmin):
     list_display = ['cidade_nome', 'uf']
     list_display_links = ['uf']
     list_select_related = ['uf']
+
+@admin.register(Bairro)
+class Bairro_admin(admin.ModelAdmin):
+    list_display = ['bairro_nome', 'cidade']
+    list_display_links = ['cidade']
+    list_select_related = ['cidade']
+
+@admin.register(Rua)
+class Rua_admin(admin.ModelAdmin):
+    list_display = ['rua_nome', 'rua_numero', 'rua_complemento', 'rua_cep', 'bairro']
+    list_display_links = ['bairro']
+    list_select_related = ['bairro']
--- site_assinaki/assinaki/models.py
+++ site_assinaki/assinaki/models.py
@@ -36,4 +36,29 @@ class Cidade(models.Model):
         verbose_name = 'Cidade'
         verbose_name_plural = "Cidades"
 
+class Bairro(models.Model):
+    bairro_nome = models.CharField('Nome do bairro', max_length=50)
+    cidade = models.ForeignKey('Cidade', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.bairro_nome
+
+    class Meta:
+        verbose_name = 'Bairro'
+        verbose_name_plural = "Bairros"
+
+class Rua(models.Model):
+    rua_nome = models.CharField('Nome da rua', max_length=150)
+    rua_numero = models.CharField('Número', max_length=10)
+    rua_complemento = models.CharField('Complemento', max_length=150)
+    rua_cep = models.CharField('CEP', max_length=50)
+    bairro = models.ForeignKey('Bairro', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return (self.rua_nome)
+
+    class Meta:
+        verbose_name = 'Rua'
+        verbose_name_plural = "Ruas"
+
 
