b5ea710d20722c4b76edaceb4d229c20a1e5a225
fernando <assinaki@hotmail.com>
2018-11-28 15:53:32 -0300

    Adicionei os campos do cliente e algumas tabelas..

--- site_assinaki/core/models.py
+++ site_assinaki/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tipo_documento(models.Model):
+    nome_tipo_documento = models.CharField('Tipo de documento', max_length=50)
+
+
+    def __str__(self):
+        return self.nome_tipo_documento
+
+    class Meta:
+        verbose_name = 'Tipo_documento'
+        verbose_name_plural = "Tipo_documentos"
