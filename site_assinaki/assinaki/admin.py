26939d0695fe9febb7b6cb04f8eb949461b1ce2c
tmenegaz <tmenegaz77@gmail.com>
2018-11-29 01:09:20 -0300

    modelos e admin

--- site_assinaki/assinaki/admin.py
+++ site_assinaki/assinaki/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Pais, Uf, Cidade, Bairro, Rua, Cliente, Pessoa_juridica, Login, Assinatura, Dados_cartao
+from .models import Pais, Uf, Cidade, Bairro,\
+    Rua, Cliente, Pessoa_juridica, Login,\
+    Assinatura, Dados_cartao, Tipo_documento, Documento
 
 
 # Register your models here.
@@ -57,7 +59,18 @@ class Assinatura_admin(admin.ModelAdmin):
     list_select_related = ['cliente']
 
 @admin.register(Dados_cartao)
-class Dados_cartao(admin.ModelAdmin):
-    list_display = ['cartao_nome','cartao_numero','cartao_data','cartao_security_cod','cartao_password','pais']
+class Dados_cartao_admin(admin.ModelAdmin):
+    list_display = ['cartao_nome', 'cartao_numero', 'cartao_data', 'cartao_security_cod', 'cartao_password', 'pais']
     list_display_links = ['pais']
     list_select_related = ['pais']
+
+@admin.register(Tipo_documento)
+class Tipo_documento_admin(admin.ModelAdmin):
+    list_display = ['nome_tipo_documento']
+
+
+@admin.register(Documento)
+class Documento_admin(admin.ModelAdmin):
+    list_display = ['documento_content','documento_data','documento_quantidade_partes','tipo_documento']
+    list_display_links = ['tipo_documento']
+    list_select_related = ['tipo_documento']
