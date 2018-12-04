ac2a37fe0aa5b22afdae48c0fd353a6f6ef5e8da
fernando <assinaki@hotmail.com>
2018-12-03 22:22:45 -0300

    Alteração

--- site_assinaki/core/admin.py
+++ site_assinaki/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from site_assinaki.assinaki.models import Assinatura
+from site_assinaki.assinaki.models import Cliente
 
-# Register your models here.
+
+class AsinaturaAdmin(admin.ModelAdmin):
+    ass = Assinatura()
+    cli = Cliente()
+  #  ass.assinatura_cod
+
+
