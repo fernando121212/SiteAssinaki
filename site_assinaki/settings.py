8566e9c997e5afa8fc46f8fd0810ad30fe9c41d4
tmenegaz <tmenegaz77@gmail.com>
2018-12-04 09:59:05 -0300

    modelos distribuidos de apps por meio de herança

--- site_assinaki/settings.py
+++ site_assinaki/settings.py
@@ -26,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '^=*)z_whz75q+o)%zk2&$n&)1o38jfgmvtr7hh1_wbqutrj^tf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
