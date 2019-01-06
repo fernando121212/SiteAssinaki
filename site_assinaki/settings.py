91ac1ab19ca7dac227d16b494301877730ab6e99
tmenegaz <tmenegaz77@gmail.com>
2019-01-06 18:19:29 -0300

    db_from_env

--- site_assinaki/settings.py
+++ site_assinaki/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DATABASES['default'] = dj_database_url.config('postgres://...', conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 django_heroku.settings(locals())
 
